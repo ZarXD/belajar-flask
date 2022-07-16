9e8a7f0e4669e0fc386d092af7e324b0d8f78c81
I'am Hads <91553852+ZarXD@users.noreply.github.com>
2022-07-16 08:16:17 +0700

    belajar banh

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import *
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+  return {
+    "status": 200,
+    "msg": "Active!!"
+  }
+
+if __name__ == "__main__":
+  app.run(debug=True)
